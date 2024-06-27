5f352d5dae985397d9c810664420737009458d05
Victoria <54598166+atkinsvh@users.noreply.github.com>
2024-06-26 22:56:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -62,4 +62,28 @@ function getWeather(latitude, longitude) {
 // DISPLAY WEATHER TO UI
 function displayWeather() {
     iconElement.src = `icons/${weather.iconId}.png`;
-    tempElement.innerHTML = `${weather.temperature.value}°<span>C
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+// C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENT
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
