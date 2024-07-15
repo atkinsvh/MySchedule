a2e2c8077711ad8f00dff9709004ac69e1360b11
Victoria <54598166+atkinsvh@users.noreply.github.com>
2024-07-15 14:52:57 -0400

    Update dailyreading.js

--- dailyreading.js
+++ dailyreading.js
@@ -1,7 +1,7 @@
-const API_KEY = '5c17c282e31380ffe54e77903f0d0880';  // Replace with your actual API key
+const API_KEY = 'YOUR_API_KEY';  // Replace with your actual API key
 
 async function fetchBibleText(book, chapter, verseStart, verseEnd) {
-    const response = await fetch(`https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/verses/${book}.${chapter}.${verseStart}-${verseEnd}`, {
+    const response = await fetch(`https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/verses/${book}.${chapter}.${verseStart}-${verseEnd}?include-chapter-numbers=false&include-verse-numbers=false`, {
         headers: {
             'api-key': API_KEY
         }
