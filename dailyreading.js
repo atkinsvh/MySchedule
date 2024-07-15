7175e40ca835cad71df53f6ac411878b371a8b70
Victoria <54598166+atkinsvh@users.noreply.github.com>
2024-07-15 14:34:59 -0400

    Update dailyreading.js

--- dailyreading.js
+++ dailyreading.js
@@ -1,5 +1,5 @@
 function loadBibleReading(fileName) {
-    fetch(fileName)
+    fetch(`kjv-bible-in-html5-master/${fileName}`)
         .then(response => response.text())
         .then(data => {
             document.getElementById("reading-content").innerHTML += data;
@@ -10,38 +10,42 @@ function loadBibleReading(fileName) {
 document.addEventListener("DOMContentLoaded", function() {
     const readingTitleElement = document.getElementById('reading-title');
     const dailyReadingFileMap = {
-        'Joshua 1:1-18': 'kjv-bible-in-html5-master/Joshua.html',
-        'Acts 21:3-15': 'kjv-bible-in-html5-master/Acts.html',
-        'Mark 1:21-27': 'kjv-bible-in-html5-master/Mark.html',
+        'Joshua 1:1-18': 'Joshua.html',
+        'Acts 21:3-15': 'Acts.html',
+        'Mark 1:21-27': 'Mark.html',
         // Add mappings for other readings as necessary
     };
 
     const psalmsFiles = [
-        'kjv-bible-in-html5-master/Psalms.html',
+        'Psalms.html',
         // Add paths for all Psalm files here
     ];
+
+    function getReadingFileNames(readingContent) {
+        const readings = readingContent.match(/Psalms: (.+), (.+)\nOld Testament: (.+)\nNew Testament: (.+)\nGospel: (.+)/);
+        if (readings) {
+            const [_, psalm1, psalm2, oldTestament, newTestament, gospel] = readings;
+            return [psalm1, psalm2, oldTestament, newTestament, gospel];
+        }
+        return [];
+    }
 
     // Load the Bible content after the readings are displayed
     displayReading();
     setTimeout(() => {
-        const readingTitle = readingTitleElement.innerText.split(' ')[1]; // Extract the Proper part
         const readingContent = document.getElementById('reading-content').innerText;
-        const readings = readingContent.match(/Psalms: (.+), (.+)\nOld Testament: (.+)\nNew Testament: (.+)\nGospel: (.+)/);
-        
-        if (readings) {
-            const [_, psalm1, psalm2, oldTestament, newTestament, gospel] = readings;
+        const [psalm1, psalm2, oldTestament, newTestament, gospel] = getReadingFileNames(readingContent);
 
-            [psalm1, psalm2].forEach(psalm => {
-                if (psalmsFiles.includes(`kjv-bible-in-html5-master/${psalm}.html`)) {
-                    loadBibleReading(`kjv-bible-in-html5-master/${psalm}.html`);
-                }
-            });
+        [psalm1, psalm2].forEach(psalm => {
+            if (psalmsFiles.includes(`${psalm}.html`)) {
+                loadBibleReading(`${psalm}.html`);
+            }
+        });
 
-            [oldTestament, newTestament, gospel].forEach(reading => {
-                if (dailyReadingFileMap[reading]) {
-                    loadBibleReading(dailyReadingFileMap[reading]);
-                }
-            });
-        }
+        [oldTestament, newTestament, gospel].forEach(reading => {
+            if (dailyReadingFileMap[reading]) {
+                loadBibleReading(dailyReadingFileMap[reading]);
+            }
+        });
     }, 500);
 });
